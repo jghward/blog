dc24d8e63a6d2342d6a5b900f2892d89cf1cd5e7
Jon Ward <jon@gamingrealms.com>
2016-10-11 20:13:59 +0100

    use Clearance to manage users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  if Clearance.configuration.allow_sign_up?
+    get "/sign_up" => "clearance/users#new", as: "sign_up"
+  end
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
 
   resources :articles do
     resources :comments
