ffe1ac7b2a358c0ca4695241f93b6371aa92aac0
Jon Ward <jon@gamingrealms.com>
2016-10-16 20:43:02 +0100

    allow session to be created when signing in

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
       resources :photos
     end
     resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-    resource :session, controller: "clearance/sessions", only: [:create]
 
     resources :users, controller: "clearance/users", only: [:create] do
       resource :password, controller: "clearance/passwords", only: [:create, :edit, :update]
@@ -36,6 +35,7 @@ Rails.application.routes.draw do
   if Clearance.configuration.allow_sign_up?
     get "/sign_up" => "clearance/users#new", as: "sign_up"
   end
+  resource :session, controller: "clearance/sessions", only: [:create]
 
   root 'welcome#index'
 end
